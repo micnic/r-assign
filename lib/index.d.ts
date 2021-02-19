f436102094eac0df544a232be91ebb2d756816ba
micnic <micnic90@gmail.com>
2021-02-20 00:47:45 +0200

    Add "useAny()"

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,9 @@
+import useAny from 'r-assign/lib/use-any';
 import useBoolean from 'r-assign/lib/use-boolean';
 import useString from 'r-assign/lib/use-string';
 
 export {
+	useAny,
 	useBoolean,
 	useString
 };
