cd4a2bd4654af2b85c485362accb0e869541f4e9
micnic <micnic90@gmail.com>
2021-02-20 00:33:40 +0200

    Add "useBoolean()"

---
+++ lib/use-boolean.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Creator of transform functions for boolean values
+ */
+declare function useBoolean(initial?: boolean): (value: any) => boolean;
+
+export = useBoolean;
---
+++ test/use-boolean.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const tap = require('tap');
+const useBoolean = require('r-assign/lib/use-boolean');
+
+tap.test('No arguments', (test) => {
+	const getBoolean = useBoolean();
+
+	test.equal(getBoolean(), false);
+	test.equal(getBoolean(true), true);
+	test.equal(getBoolean(false), false);
+	test.end();
+});
+
+tap.test('Default value provided', (test) => {
+	const getBoolean = useBoolean(true);
+
+	test.equal(getBoolean(), true);
+	test.equal(getBoolean(true), true);
+	test.equal(getBoolean(false), false);
+	test.end();
+});
+
+tap.test('Invalid default value provided', (test) => {
+	test.throws(() => {
+		useBoolean(null);
+	});
+	test.end();
+});
