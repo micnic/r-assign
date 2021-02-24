f741199e265213eb276e068259e1ea50b47cf3ac
Nicu Micleușanu <micnic90@gmail.com>
2021-02-24 13:20:24 +0200

    Add "isTypeOf()", "useOptional()" and "useTypeOf()"

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,3 +2,4 @@ export * from 'r-assign/lib/any';
 export * from 'r-assign/lib/boolean';
 export * from 'r-assign/lib/number';
 export * from 'r-assign/lib/string';
+export * from 'r-assign/lib/type';
