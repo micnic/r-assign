99b342b5d05b0458a353c59ea65197d3b76a72c3
Nicu Micleușanu <micnic90@gmail.com>
2021-02-25 21:17:11 +0200

    Refactor type tests to use destructuring from tap

--- test/type.test.js
+++ test/type.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const tap = require('tap');
+const { test } = require('tap');
 const { isBoolean } = require('r-assign/lib/boolean');
 const { isNumber } = require('r-assign/lib/number');
 const { isString, useString } = require('r-assign/lib/string');
@@ -11,100 +11,81 @@ const {
 	useValidation
 } = require('r-assign/lib/type');
 
-tap.test('isTypeOf', (test) => {
-	tap.test('Valid cases', (t) => {
-		t.ok(isTypeOf(true, [isBoolean]));
-		t.ok(isTypeOf(true, [isBoolean, isNumber]));
-		t.notOk(isTypeOf(true, [isNumber]));
-		t.end();
+test('isTypeOf', ({ end, notOk, ok, throws }) => {
+
+	ok(isTypeOf(true, [isBoolean]));
+	ok(isTypeOf(true, [isBoolean, isNumber]));
+	notOk(isTypeOf(true, [isNumber]));
+
+	throws(() => {
+		isTypeOf();
 	});
 
-	tap.test('Invalid cases', (t) => {
-		t.throws(() => {
-			isTypeOf();
-		});
-		t.throws(() => {
-			isTypeOf(null, [null]);
-		});
-		t.throws(() => {
-			isTypeOf(null, [() => null]);
-		});
-		t.end();
+	throws(() => {
+		isTypeOf(null, [null]);
 	});
 
-	test.end();
+	throws(() => {
+		isTypeOf(null, [() => null]);
+	});
+
+	end();
 });
 
-tap.test('useOptional', (test) => {
-	tap.test('No arguments', (t) => {
-		t.throws(() => {
-			useOptional();
-		});
-		t.end();
-	});
-	tap.test('Transform function provided', (t) => {
-		const getOptionalString = useOptional(useString());
+test('useOptional', ({ end, equals, throws }) => {
 
-		t.equal(typeof getOptionalString(), 'undefined');
-		t.equal(getOptionalString(''), '');
-		t.equal(getOptionalString('data'), 'data');
-		t.equal(getOptionalString(null), '');
-		t.end();
+	const getOptionalString = useOptional(useString());
+
+	equals(typeof getOptionalString(), 'undefined');
+	equals(getOptionalString(''), '');
+	equals(getOptionalString('data'), 'data');
+	equals(getOptionalString(null), '');
+
+	throws(() => {
+		useOptional();
 	});
 
-	test.end();
+	end();
 });
 
-tap.test('useTypeOf', (test) => {
+test('useTypeOf', ({ end, equals, throws }) => {
 
-	tap.test('Valid cases', (t) => {
-		const getStringOrNumber = useTypeOf('', [isNumber, isString]);
+	const getStringOrNumber = useTypeOf('', [isNumber, isString]);
 
-		t.equal(getStringOrNumber(), '');
-		t.equal(getStringOrNumber(0), 0);
-		t.equal(getStringOrNumber(1), 1);
-		t.equal(getStringOrNumber(''), '');
-		t.equal(getStringOrNumber('data'), 'data');
+	equals(getStringOrNumber(), '');
+	equals(getStringOrNumber(0), 0);
+	equals(getStringOrNumber(1), 1);
+	equals(getStringOrNumber(''), '');
+	equals(getStringOrNumber('data'), 'data');
 
-		t.end();
+	throws(() => {
+		useTypeOf();
 	});
 
-	tap.test('Invalid cases', (t) => {
-		t.throws(() => {
-			useTypeOf();
-		});
-		t.throws(() => {
-			useTypeOf(null, [null]);
-		});
-		t.throws(() => {
-			useTypeOf(null, [() => null]);
-		});
-		t.throws(() => {
-			useTypeOf(null, [isString]);
-		});
-		t.end();
+	throws(() => {
+		useTypeOf(null, [null]);
 	});
 
-	test.end();
+	throws(() => {
+		useTypeOf(null, [() => null]);
+	});
+
+	throws(() => {
+		useTypeOf(null, [isString]);
+	});
+
+	end();
 });
 
-tap.test('useValidation', (test) => {
+test('useValidation', ({ end, equals, throws }) => {
 
-	tap.test('Valid cases', (t) => {
-		const validateString = useValidation(isString);
+	const validateString = useValidation(isString);
 
-		t.equal(validateString(''), '');
-		t.end();
+	equals(validateString(''), '');
+
+	throws(() => {
+		validateString(null);
 	});
 
-	tap.test('Invalid cases', (t) => {
-		const validateString = useValidation(isString);
-
-		t.throws(() => {
-			validateString(0);
-		});
-		t.end();
-	});
-
-	test.end();
+	end();
 });
