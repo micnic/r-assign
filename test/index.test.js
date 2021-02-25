dfcf5064f9f857603ccfa14c116677cc432647e5
Nicu Micleușanu <micnic90@gmail.com>
2021-02-25 19:24:42 +0200

    Refactor index tests to use destructuring from tap

--- test/index.test.js
+++ test/index.test.js
@@ -1,65 +1,65 @@
 'use strict';
 
-const tap = require('tap');
+const { test } = require('tap');
 const rAssign = require('r-assign');
 
-tap.test('No arguments', (test) => {
-	test.throws(() => {
+test('No arguments', ({ end, throws }) => {
+	throws(() => {
 		rAssign();
 	});
-	test.end();
+	end();
 });
 
-tap.test('Invalid schema', (test) => {
-	test.throws(() => {
+test('Invalid schema', ({ end, throws }) => {
+	throws(() => {
 		rAssign({ data: null });
 	});
-	test.end();
+	end();
 });
 
-tap.test('Schema with skipped property', (test) => {
-	test.match(rAssign({ data: () => {/* Return nothing */} }), {});
-	test.end();
+test('Schema with skipped property', ({ end, match }) => {
+	match(rAssign({ data: () => {/* Return nothing */} }), {});
+	end();
 });
 
-tap.test('Schema with one property', (test) => {
-	test.match(rAssign({ data: () => null }), { data: null });
-	test.end();
+test('Schema with one property', ({ end, match }) => {
+	match(rAssign({ data: () => null }), { data: null });
+	end();
 });
 
-tap.test('Schema with inherited properties', (test) => {
+test('Schema with inherited properties', ({ end, match }) => {
 	const schema = Object.create({ prop: false });
 
 	schema.data = () => null;
 
-	test.match(rAssign(schema), { data: null });
-	test.end();
+	match(rAssign(schema), { data: null });
+	end();
 });
 
-tap.test('Schema with object applied', (test) => {
-	test.match(rAssign({
+test('Schema with object applied', ({ end, match }) => {
+	match(rAssign({
 		data: (value) => value
 	}, { data: 'data' }), { data: 'data' });
-	test.end();
+	end();
 });
 
-tap.test('Schema with object applied, property skipped', (test) => {
-	test.match(rAssign({
+test('Schema with object applied, property skipped', ({ end, match }) => {
+	match(rAssign({
 		data: () => {/* Return nothing */}
 	}, { data: 'data' }), {});
-	test.end();
+	end();
 });
 
-tap.test('Schema with two objects applied', (test) => {
-	test.match(rAssign({
+test('Schema with two objects applied', ({ end, match }) => {
+	match(rAssign({
 		data: (value) => value
 	}, { data: 'data' }, { data: 'data2' }), { data: 'data2' });
-	test.end();
+	end();
 });
 
-tap.test('Schema with two objects applied, property skipped', (test) => {
-	test.match(rAssign({
+test('Schema with two objects applied, property skipped', ({ end, match }) => {
+	match(rAssign({
 		data: () => {/* Return nothing */}
 	}, { data: 'data' }, { data: 'data2' }), {});
-	test.end();
+	end();
 });
