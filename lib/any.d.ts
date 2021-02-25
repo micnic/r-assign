31256d5ed0a6717c0f062aac94e0c7837b377d6e
Nicu Micleușanu <micnic90@gmail.com>
2021-02-25 18:14:10 +0200

    Add "validateAny()"

--- lib/any.d.ts
+++ lib/any.d.ts
@@ -3,14 +3,20 @@ import type { TransformFunction } from 'r-assign';
 /**
  * Check for non-undefined values
  */
-declare function isAny(value: any): value is Exclude<any, undefined>;
+declare const isAny: (value: any) => value is Exclude<any, undefined>;
 
 /**
  * Creator of transform functions for non-undefined values
  */
-declare function useAny(initial?: any): TransformFunction;
+declare const useAny: (initial?: any) => TransformFunction;
+
+/**
+ * Transform function to validate non-undefined values
+ */
+declare const validateAny: TransformFunction;
 
 export {
 	isAny,
-	useAny
+	useAny,
+	validateAny
 };
