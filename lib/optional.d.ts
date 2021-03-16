bcc98f4815e8a907ecd0be79bbe7b7d61a99b148
Nicu Micleușanu <micnic90@gmail.com>
2021-03-16 14:39:12 +0200

    Add isOptional() and parseOptional() methods

--- lib/optional.d.ts
+++ lib/optional.d.ts
@@ -1,4 +1,5 @@
 import type { TransformFunction } from 'r-assign';
+import type { ExtractTypeGuard, TypeGuard } from 'r-assign/lib/union';
 
 /**
  * Creator of transform functions for optional values
@@ -7,4 +8,18 @@ declare const getOptional: <T extends TransformFunction>(
 	transform: T
 ) => TransformFunction<ReturnType<T> | undefined>;
 
-export { getOptional };
+/**
+ * Check for optional values
+ */
+declare const isOptional: <T extends TypeGuard>(
+	type: T
+) => TypeGuard<ExtractTypeGuard<T> | undefined>;
+
+/**
+ * Creator of transform functions for array validation
+ */
+declare const parseOptional: <T extends TypeGuard>(
+	types: T
+) => TransformFunction<ExtractTypeGuard<T> | undefined>;
+
+export { getOptional, isOptional, parseOptional };
