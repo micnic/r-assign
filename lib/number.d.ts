340085d4ad523c7013f8e9a9df3043eed2fbdf40
Nicu Micleușanu <micnic90@gmail.com>
2021-04-13 15:22:43 +0300

    Add utility methods for working with any number values

--- lib/number.d.ts
+++ lib/number.d.ts
@@ -2,9 +2,19 @@ import type { TransformFunction } from 'r-assign';
 import type { TypeGuard } from 'r-assign/lib/union';
 
 /**
+ * Creator of transform functions for number values
+ */
+declare const getAnyNumber: (initial?: number) => TransformFunction<number>;
+
+/**
  * Creator of transform functions for finite number values
  */
 declare const getNumber: (initial?: number) => TransformFunction<number>;
+
+/**
+ * Check for number values
+ */
+declare const isAnyNumber: TypeGuard<number>;
 
 /**
  * Check for finite number values
@@ -14,6 +24,18 @@ declare const isNumber: TypeGuard<number>;
 /**
  * Transform function to validate number values
  */
+declare const parseAnyNumber: TransformFunction<number>;
+
+/**
+ * Transform function to validate finite number values
+ */
 declare const parseNumber: TransformFunction<number>;
 
-export { getNumber, isNumber, parseNumber };
+export {
+	getAnyNumber,
+	getNumber,
+	isAnyNumber,
+	isNumber,
+	parseAnyNumber,
+	parseNumber
+};
