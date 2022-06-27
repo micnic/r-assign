a4bf424a90112b4d42ad992bea63bfdf823db325
micnic <micnic90@gmail.com>
2022-06-27 19:15:52 +0300

    Add replace function for getType and parseType

--- lib/get-type.d.ts
+++ lib/get-type.d.ts
@@ -1,13 +1,19 @@
 import type { TransformFunction } from 'r-assign';
-import type { InferTypeGuard, TypeGuard } from 'r-assign/lib';
+import type {
+	InferTypeGuard,
+	NotOptionalTypeGuard,
+	ReplaceFunction,
+	TypeGuard
+} from 'r-assign/lib';
 
 /**
  * Extract values based on provided type guard and default value
  * @note Does not accept `isOptional*` type guard as it is invalid syntax
  */
 declare function getType<T extends TypeGuard>(
-	type: T,
-	initial: InferTypeGuard<T>
+	type: NotOptionalTypeGuard<T>,
+	initial: InferTypeGuard<T>,
+	replace?: ReplaceFunction<InferTypeGuard<T>>
 ): TransformFunction<InferTypeGuard<T>>;
 
 export { getType };
