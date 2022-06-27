2d014d52e032f477bad86e1b09d871dbb9e13ba4
micnic <micnic90@gmail.com>
2022-06-27 19:08:54 +0300

    Update date utility functions

--- lib/date.d.ts
+++ lib/date.d.ts
@@ -1,8 +1,35 @@
 import type { TransformFunction } from 'r-assign';
 import type { TypeGuard } from 'r-assign/lib';
 
-declare const convertToAnyDate: TransformFunction<Date>;
-declare const convertToDate: TransformFunction<Date>;
+/**
+ * Transform number or string values to date
+ */
+declare const asAnyDate: TransformFunction<Date>;
+
+/**
+ * Transform number or string values to valid date
+ */
+declare const asDate: TransformFunction<Date>;
+
+/**
+ * Transform number or string values to date
+ * @deprecated will be removed in version 2.0, use `asAnyDate()` instead
+ */
+declare function convertToAnyDate(
+	value?: unknown,
+	key?: string,
+	source?: unknown
+): Date;
+
+/**
+ * Transform number or string values to valid date
+ * @deprecated will be removed in version 2.0, use `asDate()` instead
+ */
+declare function convertToDate(
+	value?: unknown,
+	key?: string,
+	source?: unknown
+): Date;
 
 /**
  * Check for date values
@@ -14,4 +41,13 @@ declare const isAnyDate: TypeGuard<Date>;
  */
 declare const isDate: TypeGuard<Date>;
 
-export { convertToAnyDate, convertToDate, isAnyDate, isDate };
+export {
+	asAnyDate,
+	asDate,
+	convertToAnyDate,
+	convertToDate,
+	isAnyDate,
+	isAnyDate as anyDate,
+	isDate,
+	isDate as date
+};
