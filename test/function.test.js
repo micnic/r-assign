d77428a6a3fbcd9cbc59870004e83808a917587b
micnic <micnic90@gmail.com>
2022-10-12 10:16:44 +0300

    Update tests for the new structure of type guards

--- test/function.test.js
+++ test/function.test.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const { test, equal, notOk, ok, throws } = require('tap');
-const { func, isFunction } = require('r-assign/lib/function');
-const { isOptional } = require('r-assign/lib/optional');
-const { isString } = require('r-assign/lib/string');
+const {
+	func,
+	isFunction,
+	isOptional,
+	isString,
+	isTupleRestOf
+} = require('r-assign/lib');
 
 test('isFunction', ({ end }) => {
 
@@ -12,13 +16,15 @@ test('isFunction', ({ end }) => {
 	ok(isFunction([])(() => null));
 	ok(isFunction([isString])(() => null));
 	ok(isFunction([isOptional(isString)])(() => null));
+	ok(isFunction([isTupleRestOf(isString)])(() => null));
+	ok(isFunction([isTupleRestOf(isString), isString])(() => null));
 	ok(isFunction([], isString)(() => null));
 	notOk(isFunction([])(null));
 
 	throws(() => {
 		// @ts-expect-error
 		ok(isFunction([], isOptional(isString))(() => null));
-	}, TypeError('Optional type cannot be a function return'));
+	}, TypeError('Invalid use of optional type'));
 
 	end();
 });
