e1bc68020aee98478bbc90d446f5b460a15ee97a
micnic <micnic90@gmail.com>
2023-04-02 11:49:32 +0300

    Add shape for record type

--- lib/record.d.ts
+++ lib/record.d.ts
@@ -1,4 +1,25 @@
-import type { BaseTypeGuard, InferTypeGuard, TypeGuard } from 'r-assign';
+import type {
+	BaseTypeGuard,
+	InferShape,
+	InferTypeGuard,
+	Shape,
+	TypeGuard
+} from 'r-assign';
+
+/**
+ * Check for record values
+ */
+export declare function isRecordOf<V extends TypeGuard>(
+	values: BaseTypeGuard<V>
+): TypeGuard<Record<string, InferTypeGuard<V>>>;
+
+/**
+ * Check for record values
+ */
+export declare function isRecordOf<V extends TypeGuard, S extends Shape>(
+	values: BaseTypeGuard<V>,
+	shape: S
+): TypeGuard<Record<string, InferTypeGuard<V>> & InferShape<S>>;
 
 /**
  * Check for record values
@@ -14,8 +35,14 @@ export declare function isRecordOf<
 /**
  * Check for record values
  */
-export declare function isRecordOf<V extends TypeGuard>(
-	values: BaseTypeGuard<V>
-): TypeGuard<Record<string, InferTypeGuard<V>>>;
+export declare function isRecordOf<
+	K extends TypeGuard<keyof any>,
+	V extends TypeGuard,
+	S extends Shape
+>(
+	keys: K,
+	values: BaseTypeGuard<V>,
+	shape: S
+): TypeGuard<Record<InferTypeGuard<K>, InferTypeGuard<V>> & InferShape<S>>;
 
 export { isRecordOf as record };
