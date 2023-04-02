7b15b4924297385a526d6281ff5232ebb1efe712
micnic <micnic90@gmail.com>
2023-04-02 11:59:02 +0300

    Add OptionalDefaultTypeGuard to optional type overloads

--- lib/optional.d.ts
+++ lib/optional.d.ts
@@ -2,6 +2,7 @@ import type {
 	BaseTypeGuard,
 	InferTypeGuard,
 	OptionalTypeGuard,
+	OptionalDefaultTypeGuard,
 	TypeGuard
 } from 'r-assign';
 
@@ -14,6 +15,15 @@ export declare function isOptional<T extends TypeGuard>(
 ): OptionalTypeGuard<InferTypeGuard<T>>;
 
 /**
+ * Check for strict optional values with default value
+ * @note Does not accept `isOptional*` type guard as it is invalid syntax
+ */
+export declare function isOptional<T extends TypeGuard>(
+	type: BaseTypeGuard<T>,
+	def?: InferTypeGuard<T> | (() => InferTypeGuard<T>)
+): OptionalDefaultTypeGuard<InferTypeGuard<T>>;
+
+/**
  * Check for optional or undefined values
  * @note Does not accept `isOptional*` type guard as it is invalid syntax
  */
@@ -21,4 +31,16 @@ export declare function isOptionalUndefined<T extends TypeGuard>(
 	type: BaseTypeGuard<T>
 ): OptionalTypeGuard<InferTypeGuard<T> | undefined>;
 
-export { isOptional as optional, isOptionalUndefined as optionalUndef };
+/**
+ * Check for optional or undefined values with default value
+ * @note Does not accept `isOptional*` type guard as it is invalid syntax
+ */
+export declare function isOptionalUndefined<T extends TypeGuard>(
+	type: BaseTypeGuard<T>,
+	def?: InferTypeGuard<T> | (() => InferTypeGuard<T>)
+): OptionalDefaultTypeGuard<InferTypeGuard<T> | undefined>;
+
+export {
+	isOptional as optional,
+	isOptionalUndefined as optionalUndef
+};
